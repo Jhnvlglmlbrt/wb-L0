c92fb046a6996b86b626134b562c792ab180f65e
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-12-22 19:49:08 +0300

    run func

---
+++ internal/app/app.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Jhnvlglmlbrt/wb-order/config"
+	"github.com/Jhnvlglmlbrt/wb-order/internal/repository"
+	"github.com/Jhnvlglmlbrt/wb-order/package/postgres"
+)
+
+func Run(cfg *config.Config) {
+
+	// Nats connection
+
+	fmt.Println("Nats server started, connection registered")
+
+	postgresConnection, err := postgres.Connect(&cfg.PG)
+	if err != nil {
+		log.Fatalf("Error at Postgres connection: %v", err)
+	}
+	defer postgresConnection.Close()
+
+	repo := repository.NewRepository(postgresConnection)
+
+	creationTableError := repo.CreateTable()
+	if creationTableError != nil {
+		log.Fatalf("Error at table creation: %v", err)
+	}
+
+	// Cache initialization
+	// preload cache
+
+	// publish info
+
+	// subscribe and save order in db and in cache
+
+	// server init
+	// controller init
+	// get order, all orders, create order
+	// server start
+}
