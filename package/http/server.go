3d05a1a376e9dda9b2e9ac8949665927ff7696e1
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-12-24 23:52:44 +0300

    server init and routes

---
+++ package/http/server.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"github.com/Jhnvlglmlbrt/wb-order/config"
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	echo *echo.Echo
+	port string
+}
+
+func NewServer(cfg *config.HTTP) *Server {
+	return &Server{
+		echo: echo.New(),
+		port: cfg.Port,
+	}
+}
+
+func (s *Server) Start(orderHandler echo.HandlerFunc, allOrdersHandler echo.HandlerFunc) error {
+	s.echo.GET("/:order", orderHandler)
+	s.echo.GET("/", allOrdersHandler)
+	return s.echo.Start(s.port)
+}
