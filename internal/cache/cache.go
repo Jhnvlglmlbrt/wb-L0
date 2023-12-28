589e036109c23d8b985dbed3674a761690e7da6a
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-12-28 09:19:13 +0300

    deleted interface

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -9,28 +9,21 @@ import (
 	"github.com/Jhnvlglmlbrt/wb-order/internal/repository"
 )
 
-type Cache interface {
-	Init(or models.Order)
-	Preload()
-	GetOrderByUid(uid string) *models.Order
-	GetOrders() map[string]*models.Order
-}
-
-type cache struct {
+type Cache struct {
 	cache map[string]*models.Order
 	repo  repository.Repository
 	mu    sync.Mutex
 }
 
-func NewCache(repo repository.Repository) Cache {
-	return &cache{
+func NewCache(repo repository.Repository) *Cache {
+	return &Cache{
 		cache: map[string]*models.Order{},
 		repo:  repo,
 		mu:    sync.Mutex{},
 	}
 }
 
-func (c *cache) Init(or models.Order) {
+func (c *Cache) Init(or models.Order) {
 	if err := c.repo.SaveOrder(or); err != nil {
 		log.Printf("cannot insert order: %v", err)
 	}
@@ -42,25 +35,26 @@ func (c *cache) Init(or models.Order) {
 	log.Printf("Cache written: %s\n", or.OrderUid)
 }
 
-func (c *cache) Preload() {
+func (c *Cache) Preload() {
 	ords, err := c.repo.GetAll()
 	if err != nil {
 		fmt.Printf("Error at getting all data: %v\n", err)
 	}
 
-	fmt.Printf("DB len: %d\n", len(ords))
+	fmt.Printf("Database records: %d\n", len(ords))
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	for _, or := range ords {
 		c.cache[or.OrderUid] = &or
 	}
+	fmt.Printf("Cache preloaded.\n")
 }
 
-func (c *cache) GetOrderByUid(uid string) *models.Order {
+func (c *Cache) GetOrderByUid(uid string) *models.Order {
 	return c.cache[uid]
 }
 
-func (c *cache) GetOrders() map[string]*models.Order {
+func (c *Cache) GetOrders() map[string]*models.Order {
 	return c.cache
 }
